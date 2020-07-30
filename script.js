3076d9d2038dfdaf481aedba9a65d3d6e98903d0
Getúlio Salles <67875077+getuliosalles@users.noreply.github.com>
2020-07-30 08:17:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,12 @@ document.getElementById('comecar_parar').onclick = function(){
         inicio = true
         document.getElementById('comecar_parar').innerHTML = 'Parar'
 
+        if(!hora_inicio){
         hora_inicio = new Date().getTime()
+        }else{
+            hora_inicio = new Date().getTime() - time_past
+        }
+
         init_cronom = window.setInterval(function(){
             hora_atual = new Date().getTime()
             time_past = hora_atual - hora_inicio
@@ -20,4 +25,10 @@ document.getElementById('comecar_parar').onclick = function(){
         window.clearInterval(init_cronom)
         document.getElementById('comecar_parar').innerHTML = 'Começar'
     }
+}
+
+document.getElementById('zerar').onclick = function(){
+    time_past = 0
+    hora_inicio = new Date().getTime()
+    document.getElementById('cronometro').innerHTML = time_past
 }
