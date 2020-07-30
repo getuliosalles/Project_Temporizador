a2120a0f85345979bad0f339e8e0ad4312db25a3
Getúlio Salles <67875077+getuliosalles@users.noreply.github.com>
2020-07-30 08:38:07 -0300

    hora, minutos, segundo e milisegundos

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var inicio = false, hora_inicio, init_cronom, hora_atual, time_past
+var inicio = false, hora_inicio, init_cronom, hora_atual, time_past, hora, minut, seg, miliseg
 
 document.getElementById('comecar_parar').onclick = function(){
 
@@ -15,6 +15,20 @@ document.getElementById('comecar_parar').onclick = function(){
         init_cronom = window.setInterval(function(){
             hora_atual = new Date().getTime()
             time_past = hora_atual - hora_inicio
+
+            hora = Math.floor(time_past / 3600000)
+            resto = time_past - (hora * 3600000)
+
+            minut = Math.floor(resto / 60000)
+            resto -= (minut * 60000)
+
+            seg = Math.floor(resto / 1000)
+            resto -= (seg * 1000)
+
+            miliseg = resto
+
+
+
             document.getElementById('cronometro').innerHTML = time_past
 
 
