1433f2eb8f7593aa421393ec61bce82eefeb4dde
Getúlio Salles <67875077+getuliosalles@users.noreply.github.com>
2020-07-30 08:03:59 -0300

    funções começar,parar

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-var inicio = false
+var inicio = false, hora_inicio, init_cronom, hora_atual, time_past
 
 document.getElementById('comecar_parar').onclick = function(){
 
     if(!inicio){
         inicio = true
         document.getElementById('comecar_parar').innerHTML = 'Parar'
+
+        hora_inicio = new Date().getTime()
+        init_cronom = window.setInterval(function(){
+            hora_atual = new Date().getTime()
+            time_past = hora_atual - hora_inicio
+            document.getElementById('cronometro').innerHTML = time_past
+
+
+        },10)
+
     }else{
         inicio = false
+        window.clearInterval(init_cronom)
         document.getElementById('comecar_parar').innerHTML = 'Começar'
     }
 }
